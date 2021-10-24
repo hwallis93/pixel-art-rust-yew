19b7b890b6c7848d1b8a3bba2381be63eee8ea2c
Henry Wallis <henrywallis93@gmail.com>
2021-10-24 08:43:37 +0100

    Start again with Yew functional

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,18 @@
 use yew::prelude::*;
-use yewdux::prelude::*;
+use yew::{function_component, html};
 
-
-enum Action {
-    SetDimension(i32),
+#[function_component(Grid)]
+fn grid() -> Html {
+    let sum = 2 + 2;
+    let text = format!("I'm the grid {}", sum);
+    html! { <div>{text}</div> }
 }
 
-#[derive(Clone)]
-struct State {
-    dimension: i32,
-}
-
-impl Reducer for State {
-    type Action = Action;
-
-    fn reduce(&mut self, action: Self::Action) -> Changed {
-        match action {
-            Action::SetDimension(dimension) => {
-                self.dimension = dimension;
-                true
-            }
-        }
-    }
-    fn new() -> Self {
-        Self { dimension: 0}
-    }
-}
-
-mod controls;
-use controls::Controls;
-
-mod grid;
-use grid::Grid;
-
-
-type AppDispatch = DispatchProps<ReducerStore<State>>;
-
-struct App {
-    dispatch: AppDispatch
-}
-impl Component for App {
-    type Message = ();
-    type Properties = AppDispatch;
-
-    fn create(dispatch: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self {dispatch}
-    }
-
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        unimplemented!()
-    }
-
-    fn change(&mut self, dispatch: Self::Properties) -> ShouldRender {
-        self.dispatch.neq_assign(dispatch)
-    }
-
-    fn view(&self) -> Html {
-        let increment = self.dispatch.callback(|_| Action::SetDimension(5));
-        html! {
-            <div class={"AppContainer"}>
-            <button onClick=increment>{"me button"}</button>
-              <Controls/>
-              <Grid/>
-            </div>
-        }
-    }
+#[function_component(HelloWorld)]
+fn hello_world() -> Html {
+    html! { <div><p>{"Hello henry"}</p> <Grid/> </div> }
 }
 
 fn main() {
-    yew::start_app::<WithDispatch<App>>();
+    yew::start_app::<HelloWorld>();
 }
